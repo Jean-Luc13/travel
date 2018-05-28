dd9f2fa78b38a6bcaae66516f358c4fc3d1586b7
Jean-Luc13 <jeanljoubert@gmail.com>
2018-05-28 17:04:41 +0200

    added destinations page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var detinationsRouter = require('./routes/destinations');
 
 
 mongoose.connect('mongodb://jltravel:jltravel@ds137600.mlab.com:37600/travelsite');
@@ -31,6 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/destinations', detinationsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
